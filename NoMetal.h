2ed2692470e2d7f96ed985fd729674846a812d3d
Abril-Odette <abrilodettejs@gmail.com>
2025-12-04 15:47:31 -0600

    Update NoMetal.h

--- NoMetal.h
+++ NoMetal.h
@@ -5,13 +5,13 @@
     - Contiene un método para calcular un punto de ebullición simplificado.
 
     Autor: [Abril Odette Jiménez Sánchez - A01715095]
-    Fecha de creación: [2025]
-    Última modificación: [2025]
+    Fecha de creación: [03/12/2025]
+    Última modificación: [04/122025]
 */
 #ifndef NOMETAL_H
 #define NOMETAL_H
 
-#include "elemento.h" // Incluye la clase padre
+#include "elemento.h"
 
 class NoMetal : public Elemento {
 
@@ -20,10 +20,7 @@ private:
 
 public:
     /* ======================= Constructores ======================= */
-    // Constructor por defecto
     NoMetal();
-    
-    // Constructor con parámetros, llama al constructor de la clase padre
     NoMetal(int _numeroAtomico, string _nombre, string _simbolo, int _numeroMasa, vector<int> _estadosOxidacion, float _electronegatividad);
 
     /* ----------------------- Getters ----------------------- */
@@ -37,8 +34,9 @@ public:
     string obtenerInformacion() const override;
 
     /* ----------------------- Métodos ----------------------- */
-    // Calcula un punto de ebullición de manera simple y didáctica.
+    // Calcula un punto de ebullición.
     float calcularPuntoEbullicion() const;
 };
+
 
 #endif
